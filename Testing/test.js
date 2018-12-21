c65550ffc410c619a44c2fe0d81cbb8b78eef5df
penetratingshot <shadowtemplates@gmail.com>
2018-12-20 16:25:00 -0800

    .

--- Testing/test.js
+++ Testing/test.js
@@ -26,87 +26,11 @@ redisClient.on('connect', () => {
     console.log('Connected to Redis :)');
 });
 
-client.on('message', message => {
-    // Basic override command
-    redisClient.hgetall(message.guild.id, function (err, config) {
+client.on("message", message => {
+    redisClient.hgetall(message.guild.id, function (err, result) {
         if (err) throw err;
-        const adminRole = message.member.roles.find(role => role.name === config.adminRole);
-        if (message.content === "!reset") {
-            if (!adminRole) {
-                return message.reply(`you don't have the necessary role ${config.adminRole} for this command.`);
-            }
-            redisClient.hmset(message.guild.id, 'prefix', '!');
-            redisClient.hmset(message.guild.id, 'modRole', 'Mod');
-            redisClient.hmset(message.guild.id, 'adminRole', 'Admin');
-            redisClient.hmset(message.guild.id, 'on', 'true');
-            redisClient.hmset(message.guild.id, 'logChannel', '#mod-log');
-        }
-    });
-    redisClient.hgetall(message.guild.id, function(err, result) {
-        if (message.content === "!prefix") {
+        if (message.content === "!prefixpls") {
             message.channel.send(result.prefix);
-        }
-        if (!message.content.startsWith(result.prefix)) return;
-        const prefix = result.prefix;
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const command = args.shift().toLowerCase();
-        const adminRole = message.member.roles.find(role => role.name === result.adminRole);
-        const modRole = message.member.roles.find(role => role.name === result.modRole);
-        const loggingChannel = message.guild.channels.find(channel => channel.name === result.logChannel);
-        if (command === "showconfig") {
-            message.channel.send({embed: {
-                    "title": "Settings for this guild",
-                    "color": 12458242,
-                    "description": `**Prefix:** ${result.prefix}\n**Moderator Role:** ${result.modRole}\n**Administrator Role:** ${result.adminRole}\n**Logging Channel:** ${result.logChannel}\n**On**: ${result.on}`,
-                    "fields": [
-                        {
-                            "name": "Prefix",
-                            "value": `**Description:** This setting sets the global prefix for all commands on your server.`
-                        },
-                        {
-                            "name": "Moderator Role",
-                            "value": `**Description:** The setting for changing the mod role. Only difference is kick command.`
-                        },
-                        {
-                            "name": "Administrator Role",
-                            "value": `**Description:** The setting for changing any command or moderation tools.`
-                        },
-                        {
-                            "name": "Toggle On/Off",
-                            "value": "**Description:** This setting allows you control whether the main feature is on or off. This can be helpful for server admins because they don't have to deal with the bot is there are recurring errors."
-                        }
-                    ]
-            }})
-        }
-        if (command === "setconfig") {
-            if (!adminRole) {
-                return message.reply(`you don't have the necessary role ${result.adminRole} for this command.`);
-            }
-            let setting = args[0];
-            let value = args[1];
-            if (setting === "prefix") {
-                redisClient.hmset(message.guild.id, 'prefix', value);
-                message.reply(`successfully set Prefix (prefix) to ${value}`);
-            }
-            if (setting === "modRole") {
-                redisClient.hmset(message.guild.id, 'modRole', value);
-                message.reply(`successfully set Moderator Role (modRole) to ${value}`);
-            }
-            if (setting === "adminRole") {
-                redisClient.hmset(message.guild.id, 'adminRole', value);
-                message.reply(`successfully set Administrator Role (adminRole) to ${value}`);
-            }
-            if (setting === "on") {
-                redisClient.hmset(message.guild.id, 'on', value);
-                message.reply(`successfully set Toggle On/Off (on) to ${value}`);
-            }
-            if (setting === "logChannel") {
-                redisClient.hmset(message.guild.id, 'logChannel', value);
-                message.reply(`successfully set Logging Channel (logChannel) to ${value}`);
-            }
-            else {
-                message.reply(`the argument you entered is not a valid setting. Try the command ${prefix}showconf to see the available commands.`);
-            }
         }
     })
 });
