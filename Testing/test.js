fb27694d2e3577e313cd1a5aeb8a1320745eb7e1
penetratingshot <shadowtemplates@gmail.com>
2018-12-20 19:46:37 -0800

    testing

--- Testing/test.js
+++ Testing/test.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 require('dotenv').config();
 require('events').EventEmitter.defaultMaxListeners = 15;
 const client = new Discord.Client;
-const Perspective = require('perspective-api-client');
-const perspective = new Perspective({apiKey: process.env.PERSPECTIVE1})
 const redis = require('redis');
 const redisClient = redis.createClient();
 
@@ -26,13 +24,17 @@ redisClient.on('connect', () => {
     console.log('Connected to Redis :)');
 });
 
+redisClient.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+
 client.on("message", message => {
     redisClient.hgetall(message.guild.id, function (err, result) {
-        if (err) throw err;
-        if (message.content === "!prefixpls") {
-            message.channel.send(result.prefix);
+        const prefix = result.prefix;
+        if (message.content === "!yes") {
+            message.channel.send(prefix);
         }
-    })
+    });
 });
 
 client.login(process.env.DISCORD);
